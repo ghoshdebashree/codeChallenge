8cc25eea8a4f9e1172e126e11453c97f9370e8f1
ghoshdebashree <shree19ghosh@gmail.com>
2021-08-14 15:35:25 +0530

    Simple One D Array Display and Find the frequency of Chars in a string

---
+++ TwoDArray.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+public class OneDArray{
+
+     public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of rows : ");
+        int n = sc.nextInt();
+        System.out.println("Enter the size of column : ");
+        int m = sc.nextInt();
+        int[][] array = new int[n][m];
+        for(int i =0; i<n; i++){
+            for(int j=0; j< m; j++){
+              array[i][j] = sc.nextInt();
+            }
+            
+        }
+        System.out.println("Array elements are :");
+        for(int i =0 ; i<n; i++){
+          for(int j=0; j< m; j++){
+
+            System.out.print(array[i][j] + " ");
+            
+        }
+        System.out.println(" ");
+     }
+  }
+}
